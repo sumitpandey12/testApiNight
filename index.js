bb2dd6bd21524416ad483f0bab9d531d5cd42493
Sumit Pandey <sumitpandeyjnv@gmail.com>
2022-12-10 01:35:57 +0530

    mongoDBAdd

--- index.js
+++ index.js
@@ -15,4 +15,19 @@ app.use("/home", home);
 
 // connection
 const port = process.env.PORT || 9001;
-app.listen(port, () => console.log(`Listening to port ${port}`));
+// app.listen(port, () => console.log(`Listening to port ${port}`));
+
+//MongoDB
+const mdburl = 'mongodb+srv://Abhidemo03:9696858107@cluster0.jf2t9qo.mongodb.net/Student?retryWrites=true&w=majority';
+mongoose.connect(mdburl, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => app.listen(port))
+    .catch((err) => console.log(err));
+
+app.get('/index', (req, res) => {
+    Studnet.find().sort({ createdAt: 1 })
+        .then((result) => {
+            //  res.render('index',{data:result})
+            res.send(result)
+        })
+        .catch((err) => { console.log(err); })
+});
