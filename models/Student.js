f92e1381076ec7a50a61d33df32825da31c4c45d
Sumit Pandey <sumitpandeyjnv@gmail.com>
2022-12-11 17:35:17 +0530

    student model edit

--- models/Student.js
+++ models/Student.js
@@ -14,47 +14,62 @@ const Studentschema = new Schema({
         type: String,
 
     },
-    surname: {
+    rollNo: {
         type: String,
+        unique:true
 
 
     },
-    mobo: {
-        type: String,
-        required: [true, 'please enter an valid email pass'],
-        unique: true,
-        lowercase: true,
-        validate: [isEmail, 'please enter a valid email']
+    // phone: {
+    //     type: String,
+    //     required: [true, 'please enter an valid email pass'],
+    //     unique: true,
+    //     lowercase: true,
+    //     validate: [isEmail, 'please enter a valid email']
 
-    },
-    password: {
-        type: String,
-        required: [true, 'please enter an valid email pass'],
-        minlength: [5, 'min pass length is 5 charter']
+    // },
+    phone:{
+        type:String,
 
     },
 
     gender: {
         type: String,
-        possibleValues: ['blue', 'red', 'yellow']
+        possibleValues: ['blue', 'red']
 
     },
-    img: {
-        type: String
 
-    },
-    class: {
+    course: {
         type: String
     },
-    school: {
+    branch: {
         type: String
     },
-    marks: {
+    semester: {
+        type: String
+    },
+    dob: {
+        type: String
+    },
+    houseNo: {
+        type: String
+    },
+    city: {
         type: String
     },
     state: {
         type: String
-    }
+    },
+    distric: {
+        type: String
+    },
+    pin: {
+        type: String
+    },
+    landmark: {
+        type: String
+    },
+
 
 
 }, { timestamps: true });
