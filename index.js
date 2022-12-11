f0a053ac64e133409337f99e8baac2692fd0d8d6
Sumit Pandey <sumitpandeyjnv@gmail.com>
2022-12-11 17:36:49 +0530

    abc

--- index.js
+++ index.js
@@ -20,32 +20,32 @@ const port = process.env.PORT || 9001;
 //MongoDB
 const mdburl = 'mongodb+srv://Abhidemo03:9696858107@cluster0.jf2t9qo.mongodb.net/Student?retryWrites=true&w=majority';
 mongoose.connect(mdburl, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then((result) => app.listen(port))
-    .catch((err) => console.log(err));
+  .then((result) => app.listen(port))
+  .catch((err) => console.log(err));
 
 // Routes
 app.use("/home", home);
 
-app.get('/index', (req, res) => {
-    Studnet.find().sort({ createdAt: 1 })
-        .then((result) => {
-            //  res.render('index',{data:result})
-            res.send(result)
-        })
-        .catch((err) => { console.log(err); })
+app.get('/students', (req, res) => {
+  Studnet.find().sort({ createdAt: 1 })
+    .then((result) => {
+      //  res.render('index',{data:result})
+      res.send(result)
+    })
+    .catch((err) => { console.log(err); })
 });
 
-app.post('/index', (req, res) => {
-    const student = new Studnet(req.body)
-  
-    //saving it
-    student.save()
-      .then((result) => {
-        // res.redirect('/index');
-        res.send(student)
-  
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-  });
+app.post('/student', (req, res) => {
+  const student = new Studnet(req.body)
+
+  //saving it
+  student.save()
+    .then((result) => {
+      // res.redirect('/index');
+      res.send(student)
+
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+});
